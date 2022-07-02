3d7a530c14153cdb2d774067372710c67e2af5c8
Lauri <lauri.marquinez@gmail.com>
2022-07-02 13:28:31 +0200

    Activity 5 typescript

--- blogApp/src/app/components/blog/blog.component.ts
+++ blogApp/src/app/components/blog/blog.component.ts
@@ -14,17 +14,66 @@ export class BlogComponent implements OnInit {
   n_content: string = "";
   n_date: string = "";
 
-  constructor() { }
+  id: number = 3;
+
+  addNews: string = "";
+
+  constructor() {
+    this.arrNews = [
+      { id: 1, title: 'El Deportivo Alavés regresa a Primera División en San Mamés frente al Athletic (4-0)', image: "https://www.manquepierda.com/historiarealbetis/files/2018/08/Alav%C3%A9s-2016-17-400x200.jpg", content: 'Veintiún años, cinco meses y diecisiete días después de su primer descenso a Segunda -26 de marzo de 1933-, el Deportivo Alavés regresa a la máxima categoría futbolística estatal: Primera División. Lo hace en un campo mítico: San Mamés. Frente a un rival histórico: Athletic. ¡El Glorioso ha vuelto!', date: '16/05/2016' },
+      { id: 2, title: 'El Alavés solicitará nueve años y medio de prisión para Dimitry Piterman y José Nereo', image: "https://imagenes.elpais.com/resizer/01RdFghZcxDSp9_mZ5FT6CJkxl4=/414x0/arc-anglerfish-eu-central-1-prod-prisa.s3.amazonaws.com/public/WHEW2CJKKM5E56WHF3AQVWWAFM.jpg", content: 'El Deportivo Alavés anunció este miércoles que solicitará en total nueve años y medio de prisión para Dimitry Piterman y José Nereo Ruiz Vicente por "la presunta comisión de delito continuado de apropiación indebida, falsedad contable y delito societario" durante su etapa en el club vitoriano entre 2004 y 2007.', date: '29/06/2022' }
+    ]
+  }
 
   ngOnInit(): void {
+    this.drawNews();
   }
 
-  uploadNews() {
+  uploadNews(): void {
+    if (this.n_title !== "" && this.n_image !== "" && this.n_content !== "" && this.n_date !== "") {
+      let index = this.arrNews.findIndex(news => news.title === this.n_title)
+
+      if (index === -1) {
+        let newNews: News = {
+          id: this.id,
+          title: this.n_title,
+          image: this.n_image,
+          content: this.n_content,
+          date: this.n_date
+        }
+        this.arrNews.push(newNews);
+        this.id++;
+        this.drawNews();
+        this.n_title = "";
+        this.n_image = "";
+        this.n_content = "";
+        this.n_date = "";
+      }
+      else {
+        alert('Usuario duplicado')
+      }
+    } else {
+      alert('Los campos no pueden estar vacios');
+    }
 
   }
 
-  drawNews() {
-
+  drawNews(): void {
+    this.addNews = "";
+    this.arrNews.forEach((news) => {
+      this.addNews +=
+        `<article class="news-item">
+          <img src="${news.image}"
+              alt="" class="news-image">
+          <h1 class="news-title">
+            ${news.title}
+          </h1>
+          <p class="news-content">
+            ${news.content}
+          </p>
+          <span class="news-date">${news.date}</span>
+        </article>
+      `;
+    });
   }
-
 }
--- blogApp/src/app/interfaces/news.ts
+++ blogApp/src/app/interfaces/news.ts
@@ -1,5 +1,5 @@
 export interface News {
-    id: string;
+    id: number;
     title: string;
     image: string;
     content: string;
